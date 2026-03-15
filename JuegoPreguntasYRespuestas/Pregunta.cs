9c34a4ba7af619cc160ff9ce0316b975b45df6fa
Liliana Martínez Galván <lilianamtzgalvan@gmail.com>
2026-03-14 20:40:50 -0600

    Crea clases de entidad ( Categoria, Pregunta y Opcion).

---
+++ JuegoPreguntasYRespuestas/Pregunta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoPreguntasYRespuestas
+{
+    internal class Pregunta
+    {
+        public int IdPregunta { get; set; }
+        public string TextoPregunta { get; set; }
+        public string Tipo {  get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
