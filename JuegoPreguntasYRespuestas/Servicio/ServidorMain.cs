5b8210b5226e811a7447bebb33f865c40d8ccc57
Cris9029 <a366241@alumnos.uaslp.mx>
2026-04-29 22:30:28 -0600

    Se crea la clase ServidorMain y se inicializa la escucha en el puerto específico con el número dado de jugadores.
    Close #16

---
+++ JuegoPreguntasYRespuestas/Servicio/ServidorMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using JuegoPreguntasYRespuestas.DAO;
+using Newtonsoft.Json;
+
+namespace JuegoPreguntasYRespuestas.Servicio
+{
+    public static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine($"IP local: {ObtenerIPLocal()}");
+        Console.WriteLine($"Esperando jugadores...");
+        await EscucharConexionesAsync();
+    }
+    internal class ServidorMain
+    {
+        private const int Puerto = 8080;
+        private const int MaxJugadores = 3;
+        private const int TiempoPregunta = 20;
+    }
+}
