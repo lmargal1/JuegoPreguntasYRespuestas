5e21258e121235d5dd45bf11b2d33bcd6e3eddba
Liliana Martínez Galván <lilianamtzgalvan@gmail.com>
2026-04-29 21:27:56 -0600

    Crea clase Serializador.cs para covertir los objetos de C# a JSON viceversa.
    Close #36

---
+++ JuegoPreguntasYRespuestas/Servicio/Serializador.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace JuegoPreguntasYRespuestas.Servicio
+{
+    internal static class Serializador
+    {
+        //Objeto C# -> String JSON
+        public static string AJson<T>(T objeto)
+        {
+            return JsonConvert.SerializeObject(objeto);
+        }
+
+        //String JSON -> Objeto C#
+        public static T DesdeJson<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
+
+//EEEEEH intalen Newtonsoft.Json desde NuGet para que esto funcione
+/*
+ * Herramientas -> Administrador de paquetes NuGet -> Consola del administrador de paquetes
+ * Y en la consola escriben: Install-Package Newtonsoft.Json
+*/
